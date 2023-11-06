5b89c6b74088450f95a71fddcf5e6e32a71ecda6
Yanzhu Lu <z5409519@ad.unsw.edu.au>
2023-11-06 12:04:01 +0800

    update lecture

---
+++ lectures/lecture2.py
@@ -0,0 +1,27 @@
+print(1)
+
+print(2)
+
+print('John said: "Let\'s learn Python together".')
+
+print("John said: \"Let's learn Python together\".")
+
+print(6/2)
+
+a = print(0.2+0.2+0.2)  #0.6000000000000001
+
+
+print(a == 0.6)
+
+import math
+f = 0.2+0.2+0.2
+print(math.isclose(f,0.6))
+
+i = 1.0
+print(type(i))
+
+a = True
+b = 5
+
+print(f"The value of {a} and is {b}")
+print("The value of {} and is {}".format(a,b))
